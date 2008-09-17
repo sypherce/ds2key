3a25a8e2bd31aa7e9f88cc3750e40ed49227f19a
sypherce <sypherce@72c441e0-82ae-11dd-9299-37fd5bf03096>
2008-09-17 01:44:13 +0000

    1. Added Guitar Hero Grip support for both the client and the server. 2. Client's Guitar Hero Grip detection is NOT accurate. Only checks if there is ANY cart inserted.

---
+++ DS2Key.Client/arm9/source/gh.h
@@ -0,0 +1,31 @@
+#ifndef __GH_H__
+#define __GH_H__
+
+#define GH_BLUE		0x08
+#define GH_YELLOW	0x10
+#define GH_RED		0x20
+#define GH_GREEN	0x40
+#define GH_ANY		0x78
+
+#define GH_Any(pad)		(GH_Blue(pad) | GH_Yellow(pad) | GH_Red(pad) | GH_Green(pad))
+#define GH_Blue(pad)	(pad & GH_BLUE)
+#define GH_Yellow(pad)	(pad & GH_YELLOW)
+#define GH_Red(pad)		(pad & GH_RED)
+#define GH_Green(pad)	(pad & GH_GREEN)
+#define GH_keysHeld		(keysGH)
+#define GH_keysDown		((keysGH ^ oldKeysGH) & keysGH)
+#define GH_keysUp		((keysGH ^ oldKeysGH) & (~keysGH))
+
+//not accurate, reacts on any(?) cart.
+#define insertedGHPad (GBAROM[0xFFFF] == 0xF9FF)
+
+extern unsigned short int keysGH;
+extern unsigned short int oldKeysGH;
+
+extern unsigned short int keysDownGH(unsigned char color);
+extern unsigned short int keysHeldGH(unsigned char color);
+extern unsigned short int keysUpGH(unsigned char color);
+extern void initGHPad();
+extern void updateGHPad();
+
+#endif//__GH_H__
--- DS2Key.Client/arm9/source/main9.c
+++ DS2Key.Client/arm9/source/main9.c
@@ -25,6 +25,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include "gh.h"
 
 //for some odd reason I can't seem to find vcount in ndslib -sigh- (I mean, in a place it doesn't conflict with other things)
 #define		VCOUNT		(*((u16 volatile *) 0x04000006))
@@ -41,7 +42,7 @@ void Timer_50ms(void)
 //notification function to send fifo message to arm7
 void arm9_synctoarm7()//send fifo message
 {
-	REG_IPC_FIFO_TX=0x87654321;
+	REG_IPC_FIFO_TX = 0x87654321;
 }
 
 //interrupt handler to receive fifo messages from arm7
@@ -51,34 +52,33 @@ void arm9_fifo()//check incoming fifo messages
 	if(value == 0x87654321) Wifi_Sync();
 }
 
-touchPosition stylusPos;
-
 void vblfunction()
 {
 }
 
 unsigned long iptoi(char *ipchar)
 {
-	int i1=0;
-	int i2=0;
+	int i1 = 0;
+	int i2 = 0;
 	unsigned long iplong = 0;
-	while(i2<4)
+	while(i2 < 4)
 	{
-		if(*ipchar=='.' || *ipchar==0)
+		if(*ipchar == '.' || *ipchar == 0)
 		{
-			iplong = (iplong >> 8) | (i1<<24);
-			i1=0;
+			iplong = (iplong >> 8) | (i1 << 24);
+			i1 = 0;
 			i2++;
-			if(*ipchar==0 && i2<4)
+			if(*ipchar == 0 && i2 < 4)
 			{
-				iplong = iplong >> ((4-i2)*8);
-				i2=4;
+				iplong = iplong >> ((4 - i2) * 8);
+				i2 = 4;
 			}
 		}
 		else
 		{
-			if(*ipchar>='0' && *ipchar<='9') {
-				i1=i1*10+(*ipchar-'0');
+			if(*ipchar >= '0' && *ipchar <= '9')
+			{
+				i1 = i1 * 10 + (*ipchar - '0');
 			}
 		}
 		ipchar++;
@@ -89,45 +89,45 @@ unsigned long iptoi(char *ipchar)
 
 void sendCommand(int socket, struct sockaddr_in sockaddr, unsigned long ip, unsigned int port, char* command)
 {
-	sockaddr.sin_family=AF_INET;
-	sockaddr.sin_port=htons(port);
-	sockaddr.sin_addr.s_addr=ip;
-	printf("sendto... ");
-	if (sendto(socket,command,strlen(command),0,(struct sockaddr *)&sockaddr,sizeof(sockaddr))==-1)
-		printf("died\n");
+	sockaddr.sin_family = AF_INET;
+	sockaddr.sin_port = htons(port);
+	sockaddr.sin_addr.s_addr = ip;
+	if(sendto(socket, command, strlen(command), 0, (struct sockaddr*)&sockaddr, sizeof(sockaddr)) == -1)
+		printf("Sent: %s\n", command);
 	else
-		printf("lives\n");
+		printf("Failed to send: %s\n", command);
 }
 
 #define sendCommand(a) sendCommand(my_socket, sain, iptoi(address), atoi(port), a)
 
-int main(void)
+int main(int argc, char *argv[])
 {
+	{//setup system
+		videoSetMode(0);									//not using the main screen
+		videoSetModeSub(MODE_0_2D | DISPLAY_BG0_ACTIVE);	//sub bg 0 will be used to print text
+		vramSetBankC(VRAM_C_SUB_BG);
 
-	videoSetMode(0);	//not using the main screen
-	videoSetModeSub(MODE_0_2D | DISPLAY_BG0_ACTIVE);	//sub bg 0 will be used to print text
-	vramSetBankC(VRAM_C_SUB_BG);
+		SUB_BG0_CR = BG_MAP_BASE(31);
 
-	SUB_BG0_CR = BG_MAP_BASE(31);
+		BG_PALETTE_SUB[255] = RGB15(31, 31, 31);			//by default font will be rendered with color 255
 
-	BG_PALETTE_SUB[255] = RGB15(31,31,31);	//by default font will be rendered with color 255
+		//consoleInit() is a lot more flexible but this gets you up and running quick
+		consoleInitDefault((u16*)SCREEN_BASE_BLOCK_SUB(31), (u16*)CHAR_BASE_BLOCK_SUB(0), 16);
 
-	//consoleInit() is a lot more flexible but this gets you up and running quick
-	consoleInitDefault((u16*)SCREEN_BASE_BLOCK_SUB(31), (u16*)CHAR_BASE_BLOCK_SUB(0), 16);
+		irqInit();
+		irqSet(IRQ_VBLANK, vblfunction);
+		irqEnable(IRQ_VBLANK);
+	}
+	printf("DS2Key\n-\n");
 
-	iprintf("DS2Key\n-\n");
-
-	irqInit();
-	irqSet(IRQ_VBLANK, vblfunction);
-	irqEnable(IRQ_VBLANK);
 	{//send fifo message to initialize the arm7 wifi
 		REG_IPC_FIFO_CR = IPC_FIFO_ENABLE | IPC_FIFO_SEND_CLEAR;//enable & clear FIFO
 		
 		u32 Wifi_pass= Wifi_Init(WIFIINIT_OPTION_USELED);
-		REG_IPC_FIFO_TX=0x12345678;
-		REG_IPC_FIFO_TX=Wifi_pass;
+		REG_IPC_FIFO_TX = 0x12345678;
+		REG_IPC_FIFO_TX = Wifi_pass;
 		
-		*((volatile u16 *)0x0400010E) = 0;//disable timer3
+		*((volatile u16*)0x0400010E) = 0;//disable timer3
 		
 		irqInit(); 
 		irqSet(IRQ_TIMER3, Timer_50ms);//setup timer IRQ
@@ -140,44 +140,40 @@ int main(void)
 		Wifi_SetSyncHandler(arm9_synctoarm7);//tell wifi lib to use our handler to notify arm7
 
 		//set timer3
-		*((volatile u16 *)0x0400010C) = -6553;//6553.1 * 256 cycles = ~50ms;
-		*((volatile u16 *)0x0400010E) = 0x00C2;//enable, irq, 1/256 clock
+		*((volatile u16*)0x0400010C) = -6553;//6553.1 * 256 cycles = ~50ms;
+		*((volatile u16*)0x0400010E) = 0x00C2;//enable, irq, 1/256 clock
 		
-		while(Wifi_CheckInit()==0)//wait for arm7 to be initted successfully
+		while(Wifi_CheckInit() == 0)//wait for arm7 to be initted successfully
 		{
-			while(VCOUNT>192);//wait for vblank
-			while(VCOUNT<192);
+			while(VCOUNT > 192);//wait for vblank
+			while(VCOUNT < 192);
 		}
-		
 	}//wifi init complete - wifi lib can now be used!
 	
-	iprintf("Connecting via WFC data\n");
+	printf("Connecting via WFC data\n");
 	{//simple WFC connect:
-		int i;
 		Wifi_AutoConnect();//request connect
 		while(1)
 		{
-			i=Wifi_AssocStatus();//check status
-			if(i==ASSOCSTATUS_ASSOCIATED)
+			int i = Wifi_AssocStatus();//check status
+			if(i == ASSOCSTATUS_ASSOCIATED)
 			{
-				iprintf("Connected successfully!\n");
+				printf("Connected successfully!\n");
 				break;
 			}
 			if(i==ASSOCSTATUS_CANNOTCONNECT)
 			{
-				iprintf("Could not connect!\n");
+				printf("Could not connect!\n");
 				while(1);
-				break;
 			}
 		}
 	}//if connected, you can now use the berkley sockets interface to connect to the internet!
-	iprintf("-\n");
+	printf("-\n");
 
  
 	//Create a TCP socket
-	int my_socket;
-	my_socket=socket(AF_INET,SOCK_DGRAM,0);
-	iprintf("Created Socket!\n");
+	int my_socket = my_socket = socket(AF_INET, SOCK_DGRAM, 0);
+	printf("Created Socket!\n");
 
 	char *address = "192.168.1.2";
 	char *port = "9501";
@@ -185,20 +181,27 @@ int main(void)
 	//Tell the socket to connect to the IP address we found, on port 80 (HTTP)
 	struct sockaddr_in sain;
 	sain.sin_family = AF_INET;
-	sain.sin_port = htons(9501);
-	sain.sin_addr.s_addr=INADDR_ANY;
-	int i1 = 1;
-	ioctl(my_socket,FIONBIO,&i1);
-	iprintf("Connected to server!\n");
+	sain.sin_port = htons(atoi(port));
+	sain.sin_addr.s_addr = INADDR_ANY;
+	int i = 1;
+	ioctl(my_socket, FIONBIO, &i);
+	printf("Connected to server!\n");
 	//send our request
-	sendCommand("/p0");
-	printf("/p0\n");
-	int lastx = 299;
-	int lasty = 299;
+	unsigned char profile = 0;
+	char cProfile[5];
+	sprintf(cProfile, "/p%i", profile);
+	sendCommand(cProfile);
+	printf("%s\n", cProfile);
+	
+	initGHPad();
 	while(1)
 	{
+		unsigned int lastx = 299;
+		unsigned int lasty = 299;
+		touchPosition stylusPos = touchReadXY();
 		scanKeys();
-		stylusPos=touchReadXY();
+		updateGHPad();
+		
 		if(keysDown()&KEY_A) sendCommand("/ba0");
 		if(keysUp()&KEY_A) sendCommand("/ba1");
 		if(keysDown()&KEY_B) sendCommand("/bb0");
@@ -223,6 +226,14 @@ int main(void)
 		if(keysUp()&KEY_LEFT) sendCommand("/dl1");
 		if(keysDown()&KEY_RIGHT) sendCommand("/dr0");
 		if(keysUp()&KEY_RIGHT) sendCommand("/dr1");
+		if(keysDownGH(GH_BLUE)) sendCommand("/gb0");
+		if(keysUpGH(GH_BLUE)) sendCommand("/gb1");
+		if(keysDownGH(GH_YELLOW)) sendCommand("/gy0");
+		if(keysUpGH(GH_YELLOW)) sendCommand("/gy1");
+		if(keysDownGH(GH_RED)) sendCommand("/gr0");
+		if(keysUpGH(GH_RED)) sendCommand("/gr1");
+		if(keysDownGH(GH_GREEN)) sendCommand("/gg0");
+		if(keysUpGH(GH_GREEN)) sendCommand("/gg1");
 		if(keysHeld()&KEY_TOUCH && ((lastx != stylusPos.px || lasty != stylusPos.py) || (lastx == 299 || lasty == 299)))
 		{
 			char command[12];
@@ -238,13 +249,11 @@ int main(void)
 		lastx = stylusPos.px;
 		lasty = stylusPos.py;
 		//swiWaitForVBlank();
-		//iprintf(".");
 	}
-	iprintf("Sent our request!\n");
 
 	shutdown(my_socket,0);//good practice to shutdown the socket.
 	closesocket(my_socket);//remove the socket.
-	while(1);
+	//shut off DS
 
 	return 0;
 }
