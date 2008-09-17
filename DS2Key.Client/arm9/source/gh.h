82943a149a471325bc05d599f93e9e88b57721b5
sypherce <sypherce@72c441e0-82ae-11dd-9299-37fd5bf03096>
2008-09-17 18:13:30 +0000

    1. Just some code cleanups, removals and syntax.

--- DS2Key.Client/arm9/source/gh.h
+++ DS2Key.Client/arm9/source/gh.h
@@ -5,7 +5,6 @@
 #define GH_YELLOW	0x10
 #define GH_RED		0x20
 #define GH_GREEN	0x40
-#define GH_ANY		0x78
 
 #define GH_Any(pad)		(GH_Blue(pad) | GH_Yellow(pad) | GH_Red(pad) | GH_Green(pad))
 #define GH_Blue(pad)	(pad & GH_BLUE)
@@ -16,7 +15,6 @@
 #define GH_keysDown		((keysGH ^ oldKeysGH) & keysGH)
 #define GH_keysUp		((keysGH ^ oldKeysGH) & (~keysGH))
 
-//not accurate, reacts on any(?) cart.
 #define insertedGHPad (GBAROM[0xFFFF] == 0xF9FF)
 
 extern unsigned short int keysGH;
