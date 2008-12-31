a20324c96187cb56ee01de9b280bca166d50eef5
sypherce <sypherce@72c441e0-82ae-11dd-9299-37fd5bf03096>
2008-12-31 03:41:34 +0000

    1. Added GPL license to the top of files missing it.

--- trunk/DS2Key.Client/arm9/source/keyboard.c
+++ trunk/DS2Key.Client/arm9/source/keyboard.c
@@ -1,3 +1,20 @@
+/*
+DS2Key Client - An application to use your DS as a PC Gamepad
+Copyright (C) 2008  Derrick (sypherce) Wirth
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <nds.h>
 #include <stdio.h>
 #include <string.h>
