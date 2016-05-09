790c93ab4cce4056b314665fc3a9ee00f2dc1efe
Derrick Wirth <sypherce@gmail.com>
2016-05-08 22:29:26 -0700

    Removed unnecessary function

--- client/source/core.cpp
+++ client/source/core.cpp
@@ -28,49 +28,6 @@ bool input_changed = false;
 bool enable_input_timeout = true;
 bool toggle_both_lights = false;
 
-#ifdef _3DS
-void displayKeys()
-{
-	static char keysNames[32][32] = {
-		"KEY_A", "KEY_B", "KEY_SELECT", "KEY_START",
-		"KEY_DRIGHT", "KEY_DLEFT", "KEY_DUP", "KEY_DDOWN",
-		"KEY_R", "KEY_L", "KEY_X", "KEY_Y",
-		"", "", "KEY_ZL", "KEY_ZR",
-		"", "", "", "",
-		"KEY_TOUCH", "", "", "",
-		"KEY_CSTICK_RIGHT", "KEY_CSTICK_LEFT", "KEY_CSTICK_UP", "KEY_CSTICK_DOWN",
-		"KEY_CPAD_RIGHT", "KEY_CPAD_LEFT", "KEY_CPAD_UP", "KEY_CPAD_DOWN"
-	};
-	static u32 kDownOld = 0;
-	static u32 kHeldOld = 0;
-	static u32 kUpOld = 0;
-
-	u32 kDown = hidKeysDown();
-	u32 kHeld = hidKeysHeld();
-	u32 kUp = hidKeysUp();
-	if (kDown != kDownOld || kHeld != kHeldOld || kUp != kUpOld)
-	{
-		consoleClear();
-
-		printf("\x1b[3;0H");
-		for(int i = 0; i < 32; i++)
-		{
-			if (kDown & BIT(i)) printf("%s down\n", keysNames[i]);
-			if (kHeld & BIT(i)) printf("%s held\n", keysNames[i]);
-			if (kUp & BIT(i)) printf("%s up\n", keysNames[i]);
-		}
-		kDownOld = kDown;
-		kHeldOld = kHeld;
-		kUpOld = kUp;
-	}
-	circlePosition pos;
-	hidCircleRead(&pos);
-	printf("\x1b[2;0H%04d; %04d", pos.dx, pos.dy);
-	printf("\x1b[4;0H%04d; %04d", g_stylus_position.px, g_stylus_position.py);
-	printf("\x1b[8;0HHold L + R + START + SELECT to exit");
-}
-#endif
-
 char* GetTime()
 {
 	static char s_time_char[12];
@@ -291,7 +248,6 @@ void DeInit()
 int Loop()
 {
 #if defined(_3DS)
-	displayKeys();
 	if(
 	(keysHeld()&KEY_START)&&
 	(keysHeld()&KEY_SELECT)&&
