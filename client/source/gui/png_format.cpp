7e73e29bee2bb5d021e69cf4694b245c2f1a8578
Derrick Wirth <sypherce@gmail.com>
2016-08-15 01:42:32 -0700

    Fixed bad formatting

--- client/source/gui/png_format.cpp
+++ client/source/gui/png_format.cpp
@@ -9,40 +9,40 @@
 namespace D2K {namespace GUI{
 
 bool LoadPngImage(char *filename, int &width, int &height, unsigned char **output)
-{	
+{
 #if defined(_3DS)
 	// Open png file
-    FILE *file = fopen(filename, "rb");
-    if(file == nullptr)
+	FILE *file = fopen(filename, "rb");
+	if(file == nullptr)
 	{
 		LOG(ERROR) << "Issue reading " << filename << ".";
-        return false;
+		return false;
 	}
- 
-    // Initialize png read struct
-    png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
-    if(png_ptr == nullptr)
+
+	// Initialize png read struct
+	png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
+	if(png_ptr == nullptr)
 	{
 		// Close file
-        fclose(file);
+		fclose(file);
 		LOG(ERROR) << "Issue initialize png read struct from " << filename << ".";
-        return false;
-    }
- 
+		return false;
+	}
+
 	// Initialize png info struct
-    png_infop info_ptr = png_create_info_struct(png_ptr);
-    if(info_ptr == nullptr)
+	png_infop info_ptr = png_create_info_struct(png_ptr);
+	if(info_ptr == nullptr)
 	{
 		// Deinitialize png read struct
-        png_destroy_read_struct(&png_ptr, nullptr, nullptr);
+		png_destroy_read_struct(&png_ptr, nullptr, nullptr);
 		// Close file
-        fclose(file);
+		fclose(file);
 
 		LOG(ERROR) << "Issue initializing png info struct from " << filename << ".";
-        return false;
-    }
- 
-    // Setup libpng error handling
+		return false;
+	}
+
+	// Setup libpng error handling
 	{
 		if(setjmp(png_jmpbuf(png_ptr)))
 		{
@@ -55,46 +55,46 @@ bool LoadPngImage(char *filename, int &width, int &height, unsigned char **outpu
 			return false;
 		}
 	}
- 
-    // Setup libpng IO
-    png_init_io(png_ptr, file);
- 
-    // Set libpng sig bytes
-    unsigned int sig_read = 0;
-    png_set_sig_bytes(png_ptr, sig_read);
- 
-    // read the entire PNG into memory
+
+	// Setup libpng IO
+	png_init_io(png_ptr, file);
+
+	// Set libpng sig bytes
+	unsigned int sig_read = 0;
+	png_set_sig_bytes(png_ptr, sig_read);
+
+	// read the entire PNG into memory
 	// PNG_TRANSFORM_STRIP_16   : Strip 16-bit samples to 8 bits
 	// PNG_TRANSFORM_PACKING    : Expand 1, 2 and 4-bit samples to bytes
 	// PNG_TRANSFORM_EXPAND     : Expand paletted images to RGB, grayscale to 8-bit images and tRNS chunks to alpha channels
 	// PNG_TRANSFORM_BGR        : Flip RGB to BGR, RGBA to BGRA
 	// PNG_TRANSFORM_STRIP_ALPHA: Discard the alpha channel
-    png_read_png(png_ptr, info_ptr, PNG_TRANSFORM_STRIP_16 | PNG_TRANSFORM_PACKING | PNG_TRANSFORM_EXPAND | PNG_TRANSFORM_BGR | PNG_TRANSFORM_STRIP_ALPHA, nullptr);
- 
+	png_read_png(png_ptr, info_ptr, PNG_TRANSFORM_STRIP_16 | PNG_TRANSFORM_PACKING | PNG_TRANSFORM_EXPAND | PNG_TRANSFORM_BGR | PNG_TRANSFORM_STRIP_ALPHA, nullptr);
+
 	// Read some variables, width, height, depth...
-    int bit_depth, color_type, interlace_type;
-    png_get_IHDR(png_ptr, info_ptr, (png_uint_32*)&width, (png_uint_32*)&height, &bit_depth, &color_type,
-                 &interlace_type, nullptr, nullptr);
+	int bit_depth, color_type, interlace_type;
+	png_get_IHDR(png_ptr, info_ptr, (png_uint_32*)&width, (png_uint_32*)&height, &bit_depth, &color_type,
+	             &interlace_type, nullptr, nullptr);
 
 	// Allocate the output data
 	uint32_t row_bytes = png_get_rowbytes(png_ptr, info_ptr);
-    *output = (unsigned char*) malloc(row_bytes * height);
+	*output = (unsigned char*) malloc(row_bytes * height);
 
 	// Copy the png file to the output
-    png_bytepp row_pointers = png_get_rows(png_ptr, info_ptr);
-    for(int y = 0; y < height; y++)
+	png_bytepp row_pointers = png_get_rows(png_ptr, info_ptr);
+	for(int y = 0; y < height; y++)
 	{
-        memcpy(*output+(row_bytes * y), row_pointers[y], row_bytes);
-    }
- 
+		memcpy(*output+(row_bytes * y), row_pointers[y], row_bytes);
+	}
+
 	// Deinitialize png read struct
-    png_destroy_read_struct(&png_ptr, &info_ptr, nullptr);
- 
+	png_destroy_read_struct(&png_ptr, &info_ptr, nullptr);
+
 	// Close file
-    fclose(file);
- 
+	fclose(file);
+
 	LOG(INFO) << "Loaded " << filename << ".";
-    return true;
+	return true;
 #elif defined(_NDS)
 	return false;
 #endif
