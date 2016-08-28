8bb771223f590338c9cf0bc0b29e6c1369bd5b0f
Derrick Wirth <sypherce@gmail.com>
2016-08-28 01:01:21 -0700

    Re-arranged #ifdefs for intellisense

--- common/misc.cpp
+++ common/misc.cpp
@@ -3,16 +3,16 @@
 
 namespace D2K {
 
-#ifdef WIN32GUI
-const std::string VERSION_STRING = "DS2Key (GUI) git Build";
-#elif defined(_WIN32)
-const std::string VERSION_STRING = "DS2Key (Console) git Build";
-#elif defined(__linux__)
-const std::string VERSION_STRING = "DS2Key (Linux) git Build";
-#elif defined(_NDS)
+#if defined(_NDS)
 const std::string VERSION_STRING = "DS2Key (DS) git Build";
 #elif defined(_3DS)
 const std::string VERSION_STRING = "DS2Key (3DS) git Build";
+#elif defined(__linux__)
+const std::string VERSION_STRING = "DS2Key (Linux) git Build";
+#elif defined(WIN32GUI)
+const std::string VERSION_STRING = "DS2Key (GUI) git Build";
+#elif defined(_WIN32)
+const std::string VERSION_STRING = "DS2Key (Console) git Build";
 #endif
 
 std::string ltos(long i)
@@ -40,7 +40,7 @@ uint8_t string_to_uint8_t(std::string str)
 {
 	long return_value = D2K::stol(str);
 	// Clamp to uint8_t
-	if (return_value > 255
+	if(return_value > 255
 	|| return_value < 0)
 		return_value = 0;
 
@@ -51,7 +51,7 @@ uint16_t string_to_uint16_t(std::string str)
 {
 	long return_value = D2K::stol(str);
 	// Clamp to uint16_t
-	if (return_value > 65535
+	if(return_value > 65535
 	|| return_value < 0)
 		return_value = 0;
 
