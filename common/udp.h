ed45a1855bef7ed1f36f26b791b2dacc7f9dfe19
Derrick Wirth <sypherce@gmail.com>
2016-09-13 15:25:14 -0700

    Altered comment syntax

--- common/udp.h
+++ common/udp.h
@@ -39,46 +39,46 @@ void Init();
 // Disconnects UDP if connected and Cleans up Winsock
 void DeInit();
 
-// @return (true) if connected, else (false)
+// return: (true) if connected, else (false)
 bool IsConnected();
 
 // connect udp system
-// @return (0) if connected, else (errno)
+// return: (0) if connected, else (errno)
 int Connect();
 // connect udp system
-// @param _port Port to connect to. if port 0, SetConfigPort defaults to 9501
-// @return (0) if connected, else (errno)
+//  param: _port Port to connect to, if port 0, SetConfigPort defaults to 9501
+// return (0) if connected, else (errno)
 int Connect(uint16_t _port);
 // connect udp system
-// @param _non_blocking false == blocking, true == non-blocking
-// @param _port Port to connect to. if port 0, SetConfigPort assigns to default port 9501
-// @return (0) if connected, else (errno)
+//  param: _non_blocking false == blocking, true == non-blocking
+//  param: _port Port to connect to, if port 0, SetConfigPort assigns to default port 9501
+// return: (0) if connected, else (errno)
 int Connect(bool _non_blocking, uint16_t _port);
 
 // disconnect udp system
-// @return (0) without error, else (errno)
+// return: (0) without error, else (errno)
 int Disconnect();
 
 // sends contents of (buffer) up to (length) in size
-// @param buffer Pointer to data
-// @param length Length of data to send
-// @return (0) without error, (-1) not connected, (-2) invalid length, (-3) invalid pointer, else (errno)
+//  param: buffer Pointer to data
+//  param: length Length of data to send
+// return: (0) without error, (-1) not connected, (-2) invalid length, (-3) invalid pointer, else (errno)
 int Send(const void* buffer, unsigned int length);
 
 // receives contents into (buffer) up to (length) in size
-// @param buffer Pointer to data.
-//       (buffer) must be allocated before calling.
-//       (buffer) is only modified up to (length) if recvfrom is successful, otherwise it should be left un modified.
-// @param length Length of data to receive
-// @return (0) without error, (-1) not connected, (-2) invalid length, (-3) invalid pointer, (-4) NULL _remote_sockaddr, else (errno)
+//  param: buffer Pointer to data
+//         buffer must be allocated before calling
+//         buffer is only modified up to (length) if recvfrom is successful, otherwise it should be left un-modified
+//  param: length Length of data to receive
+// return: (0) without error, (-1) not connected, (-2) invalid length, (-3) invalid pointer, (-4) NULL _remote_sockaddr, else (errno)
 int Recv(void* buffer, unsigned int length);
 // Receives contents into (buffer) up to (length) in size
-// @param buffer Pointer to data.
-// (buffer) must be allocated before calling.
-// (buffer) is only modified up to (length) if recvfrom is successful, otherwise it should be left un modified.
-// @param length Length of data to receive
-// @param _remote_sockaddr Container for recvfrom to fill in with remote address
-// @return (0) without error, (-1) not connected, (-2) invalid length, (-3) invalid pointer, (-4) NULL _remote_sockaddr, else (errno)
+//  param: buffer Pointer to data
+//         buffer must be allocated before calling
+//         buffer is only modified up to (length) if recvfrom is successful, otherwise it should be left un modified
+//  param: length Length of data to receive
+//  param: _remote_sockaddr Container for recvfrom to fill in with remote address
+// return: (0) without error, (-1) not connected, (-2) invalid length, (-3) invalid pointer, (-4) NULL _remote_sockaddr, else (errno)
 int Recv(void* buffer, unsigned int length, struct sockaddr* _remote_sockaddr);
 
 unsigned long GetLocalIP();
@@ -95,9 +95,15 @@ std::string GetPortString();
 
 // set port to connect to
 // note: we use SetConfigPort to avoid clash with SetPort defined in windows
-// @param _port UDP port between 1-65535. If port is 0, we default to 9501
+//  param: _port UDP port between 1-65535. If port is 0, we default to 9501
 void SetConfigPort(const std::string& _port);
+// set port to connect to
+// note: we use SetConfigPort to avoid clash with SetPort defined in windows
+//  param: _port UDP port between 1-65535. If port is 0, we default to 9501
 void SetConfigPort(const char* _port);
+// set port to connect to
+// note: we use SetConfigPort to avoid clash with SetPort defined in windows
+//  param: _port UDP port between 1-65535. If port is 0, we default to 9501
 void SetConfigPort(uint16_t _port);
 
 #if defined(D2KCLIENT)
@@ -105,7 +111,7 @@ void SetConfigPort(uint16_t _port);
 void SendNormalSetting(DS2KeyNormalSettingsPacket setting);
 
 // sends a command packet
-// @param command Value between 0 - 11, 12 - 255 are ignored
+//  param: command Value between 0 - 11, 12 - 255 are ignored
 void SendCommand(uint8_t command);
 
 // updates current gamepad status
